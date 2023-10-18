0bcb84375f34d1b1a6393cfdea834bc41a6bee55
BBTibber <youngsuk0311@gmail.com>
2023-10-18 11:57:48 +0000

    sum_squares_even commit

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from typing import List
 # Skeleton code for even_list
 def even_list(int_list: List[int]) -> List[int]:
-    pass
+    return_list = []
+    for i in int_list:
+        if i%2==0:
+           return_list.append(i)
+    return return_list
+
 # Skeleton code for sum_of_squares_of_even
 def sum_of_squares_of_even(even_int_list: List[int]) -> int:
-    pass
+    return_int = 0
+    for i in even_int_list:
+        return_int += (i**2)
+    return return_int
+
 
 def main():
 # Example list
